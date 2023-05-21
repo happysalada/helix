b5bfb34ffe805ee6e2c7b316f393090790fccdf4
happysalada <raphael@megzari.com>
2023-05-21 11:32:27 -0400

    language: add surrealql

---
+++ runtime/queries/surql/highlights.scm
@@ -0,0 +1,18 @@
+
+(keyword) @keyword
+(string) @string
+(number) @number
+(punctuation) @punctuation 
+(operator) @operator
+(variable) @type
+(constant) @constant.builtin
+(function) @function
+(bool) @boolean
+(nothing) @type
+(comment) @comment
+(record) @type
+(function) @function
+(property) @field
+(identifier) @identifier
+(casting) @conceal
+(duration) @number
